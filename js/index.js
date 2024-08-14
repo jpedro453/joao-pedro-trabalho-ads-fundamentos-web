5a5d0ca283d9c30d14e32f5ac7e768022fe3bb6f
jpedro453 <jpedroswc453@gmail.com>
2024-08-14 17:10:27 -0300

    refactor: remove change of tabs

--- js/index.js
+++ js/index.js
@@ -1,24 +1,18 @@
+// Pega todos os links com classe nav_link
 const links = document.querySelectorAll('.nav_link')
-const sections = document.querySelectorAll('section')
 
 links.forEach((link) => {
+    // Espera pelo clique em algum dos links
     link.addEventListener('click', (event) => {
         // Remove a classe 'active' de todos os links e seções
         links.forEach((link) => link.classList.remove('active'))
-        sections.forEach((section) => section.classList.remove('active'))
 
-        // Pega o href do link clicado
-        const link_href = event.currentTarget.getAttribute('href')
-
-        // Separa o # do href
-        const section_to_show = link_href.split('#')[1]
-
-        // Adiciona a classe active para section correta e para o link atual
-        document.getElementById(section_to_show).classList.add('active')
+        //Adiciona a classe active no link clicado para estilização
         event.currentTarget.classList.add('active')
     })
 })
 
+// Pega o form de acordo com o id
 const form = document.getElementById('form')
 form.addEventListener('submit', (event) => {
     // Exibir uma mensagem pro usuário ao enviar o formulário
